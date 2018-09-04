05c2b99cd46fdd47c641602bf500ccb894c95371
mdcollins80 <mdcollins80@gmail.com>
2018-09-04 14:11:54 -0400

    Removes unused fromAuth import

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,4 @@
 import { RSAA } from 'redux-api-middleware';
-import { withAuth } from '../reducers'
 
 export const LOGIN_REQUEST = '@@auth/LOGIN_REQUEST';
 export const LOGIN_SUCCESS = '@@auth/LOGIN_SUCCESS';
