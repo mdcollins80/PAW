db0fa0a5125097383686ab685f12f0c23ec97900
mdcollins80 <mdcollins80@gmail.com>
2018-09-14 22:36:58 -0400

    Adds userpick get request for picks page

--- src/containers/Picks/Picks.js
+++ src/containers/Picks/Picks.js
@@ -30,8 +30,18 @@ class Picks extends Component {
   state = {
     weeks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
     games: null,
-    selectedWeek: 1
+    selectedWeek: 1,
+    picks: null
   }
+  
+  // make the team names clickable.  onClick, check if a pick exists for this
+  // game already.  if it doesn't, POST request to select that team to win.
+  // if it does, PATCH request to update the pick.  POST and PATCH if and only 
+  // if it is NOT past kickoff time.
+  // pull the userpicks and highlight the teams they have chosen.
+  // when there is a winner for the game, highlight the row in green for a match
+  // red for a wrong pick
+  
   
   componentDidMount () {
     const config = {
@@ -39,15 +49,22 @@ class Picks extends Component {
     }
     
     if (!JSON.parse(localStorage.getItem('games'))) {
-      axios.get(process.env.REACT_APP_API_URL + `/api/games`, config)
-        .then(response => {
-          this.setState({games: response.data})
-        })
+      axios.all([this.axiosGetRequest('/api/games/', config), this.axiosGetRequest('api/userpicks/', config)])
+        .then(axios.spread((games, picks) => {
+          this.setState({games: games.data, picks: picks.data})
+        }))
         .then(() => localStorage.setItem('games', JSON.stringify(this.state.games)))
         .catch(error => console.log(error))
     } else {
+      this.axiosGetRequest('/api/userpicks/', config)
+        .then(response => this.setState({picks: response.data}))
+        .catch(error => console.log(error))
       this.setState({games: JSON.parse(localStorage.getItem('games'))})
     }
+  }
+  
+  axiosGetRequest = (url, config) => {
+    return axios.get(process.env.REACT_APP_API_URL + url, config)
   }
   
   weekSelect = (week) => {
